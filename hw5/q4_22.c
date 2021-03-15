94154697953c32636caf0e2d8cf774d6f423fd18
Raymond Chin <chin.raymond.andrew@gmail.com>
2021-03-15 00:33:46 -0700

    fixed pointer and syntax errors in thread functions; debug lines present

--- hw5/q4_22.c
+++ hw5/q4_22.c
@@ -1,4 +1,4 @@
-#include <iostream>
+//#include <iostream.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,24 +16,26 @@ void *find_average(void *int_arr);
 void *find_minimum(void *int_arr);
 void *find_maximum(void *int_arr);
 
-int main(int argc, int *argv[]) {
+int main(int argc, char *argv[]) {
 	pthread_t workers[NUMBER_OF_THREADS];
-	int size = argc-1;
-	int input_arr[size];
+	int *input_arr = (int *)malloc((size)*sizeof(int));
 
 	// Transfer trimmed inputs from command line to array
-	for(int i = 0; i <= size; i++) {
-		input_arr[i] = atoi(argv[i+1]);
+	for(int i = 1; i < argc; i++) {
+		printf("inputting %d\n", atoi(argv[i]));
+		input_arr[i-1] = atoi(argv[i]);
+		size++;
 	}
 	
-	for(int i = 0; i <= size; i++) {
+	for(int i = 0; i < size; i++) {
 		printf("%d ", input_arr[i]);
 	}
+	printf("\n");
 
 	// Create a new thread for each statistic function
-	pthread_create(workers[0], 0, find_average, &input_arr);
-	pthread_create(workers[1], 0, find_minimum, &input_arr);
-	pthread_create(workers[2], 0, find_maximum, &input_arr);
+	pthread_create(&workers[0], 0, find_average, (void *)input_arr);
+	pthread_create(&workers[1], 0, find_minimum, (void *)input_arr);
+	pthread_create(&workers[2], 0, find_maximum, (void *)input_arr);
 
 	// Wait for all threads to finish
 	for(int i = 0; i < NUMBER_OF_THREADS; i++) {
@@ -49,6 +51,7 @@ int main(int argc, int *argv[]) {
 }
 
 void *find_average(void *int_array) {
+	printf("avg thread executing\n");
 	int *arr = (int *)int_array;
 	int sum = 0;
 
@@ -57,31 +60,38 @@ void *find_average(void *int_array) {
 	}
 
 	average = sum/size;
+	printf("avg thread exiting\n");
 	pthread_exit(0);
 }
 
 void *find_minimum(void *int_array) {
 	int *arr = (int *)int_array;
-	int curr_max = arr[0];
+	int curr_min = arr[0];
 
+	printf("minimum thread executing\n");
 	for(int i = 1; i < size; i++) {
-		if(arr[i] > curr_max) {
-			curr_max = arr[i];
+		if(arr[i] < curr_min) {
+			curr_min = arr[i];
 		}
 	}
 
-	maximum = curr_max;
+	minimum = curr_min;
+	printf("minimum thread exiting\n");
+	pthread_exit(0);
 }
 
-void *find_minimum(void *int_array) {
+void *find_maximum(void *int_array) {
         int *arr = (int *)int_array;
-        int curr_min = arr[0];
+        int curr_max = arr[0];
 
+	printf("maximum thread executing\n");
 	for(int i = 1; i < size; i++) {
-                if(arr[i] < curr_min) {
-                        curr_min = arr[i];
+                if(arr[i] > curr_max) {
+                        curr_max = arr[i];
                 }
 	}
 
-	minimum = curr_min;
+	maximum = curr_max;
+	printf("maximum thread exiting\n");
+	pthread_exit(0);
 }
